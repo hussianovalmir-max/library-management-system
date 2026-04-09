ef24ec080481a9a64a017377f59fd59b5564f32e
hussianovalmir-max <hussianovalmir@gmail.com>
2026-04-09 09:11:27 +0500

    Update book.py

--- src/book.py
+++ src/book.py
@@ -1,9 +1,12 @@
 class Book:
-    def __init__(self, title, author, isbn):
+    def __init__(self, title: str, author: str, isbn: str) -> None:
         self.title = title
         self.author = author
         self.isbn = isbn
-        self.is_available = True
+        self.is_available: bool = True
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.title} by {self.author}"
+
+    def __repr__(self) -> str:
+        return f"Book('{self.title}', '{self.author}', '{self.isbn}')"
